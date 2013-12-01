f10d2bde1de7584253a8f3ed58f767838d01d243
Pratik Sinha <pratik@humbug.in>
2013-12-01 12:09:02 +0530

    Made the tool to sort according to non alphabetic characters more generic

--- nr_sort_twitter_ids_number_of_numeric_non_alphabets.sh
+++ nr_sort_twitter_ids_number_of_numeric_non_alphabets.sh
@@ -18,11 +18,27 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
+if [ "x$2" != "x" ]; then
+	OUTPUT=$2
+else
+	OUTPUT="$(basename $0 .sh).csv"
+fi
 
-cat twitterids.csv | while read line
+if [ "x$1" != "x" ]; then
+	mydir=$(mktemp -dt "twanalysis.XXXXXXXXXX")
+	FILE=$1
+	cat $FILE | cut -d ',' -f '1' > $mydir/$OUTPUT
+	FILE=$mydir/$OUTPUT
+else
+	FILE=twitterids.csv
+fi
+
+echo $FILE $OUTPUT
+
+cat $FILE | while read line
 do
 	twid=$line
 	idWithoutAlphabets=$(echo $twid | tr -d [:alpha:])
 	lengthNonAlphabets=$(echo $idWithoutAlphabets | wc -c)
 	echo "$lengthNonAlphabets,$twid"
-done | sort -t ',' -k 1rn,1 | cut -d ',' -f 2 > sort_twitter_ids_number_of_numeric_non_alphabets.csv
+done | sort -t ',' -k 1rn,1 | cut -d ',' -f 2 > $OUTPUT
