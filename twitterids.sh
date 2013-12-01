16626964657a613f0d84a89f63f89bec0eda6a84
Pratik Sinha <pratik@humbug.in>
2013-12-01 08:40:58 +0530

    Generating a list of followers

---
+++ twitterids.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  export-csv.sh
+# 
+#         USAGE:  ./export-csv.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Pratik Sinha (freethinker), pratik@humbug.in
+#       COMPANY: http://www.humbug.in/
+#       CREATED: 11/29/2013 10:34:10 PM IST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+#!/bin/bash
+sqlite3 ./scraperwiki.sqlite <<!
+.headers on
+.mode csv
+.output twitterids.csv
+select screen_name from twitter_followers order by following_count;
+!
